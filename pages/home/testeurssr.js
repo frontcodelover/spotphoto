be56bdcf3aaa29b8252353668a34fa144fd5e263
frontcodelover <contact@avantjetaisriche.com>
2022-05-25 21:56:48 +0200

    Query doesn't work

---
+++ pages/home/testeurssr.js
@@ -0,0 +1,26 @@
+import { db } from "../firebase/firebase";
+import React from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import Coucou from "../../components/coucou";
+
+export async function getStaticProps() {
+    const spotsCollectionRef = collection(db, "spots");
+    const q = query(spotsCollectionRef, where('pays', 'in', ['inputs']));
+
+  const data = await getDocs(q);
+  const spots = data?.docs?.map((doc) => ({
+    data: doc.data(),
+    id: doc.id,
+    uid: doc.data().uid,
+  }));
+
+  return {
+    props: {
+      spots: spots || null,
+    },
+  };
+}
+
+export default function Testeurssr({ spots }) {
+  return <Coucou spots={spots} />;
+}
